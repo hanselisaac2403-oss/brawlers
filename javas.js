1ccb1a3b8c3ff5e23b307eefd0508b9b950126c7
hansel <hanselisaac2403@gmail.com>
2026-05-19 14:39:28 -0500

    actualizacion

--- javas.js
+++ javas.js
@@ -1,1 +1,21 @@
-console.log("sigma papiador")
+let nombre ="hansel"
+
+
+
+
+const saludar = (nombre) =>{
+ console.log("hola "+ nombre+"cuanto años tienes ")
+}
+
+const brawlers = ["Spike", "Crow", "Sandy", "Amber"];
+brawlers.forEach((brawler) => {
+  console.log(brawler, "está listo para la batalla!");
+});
+//for(let i = 0; i < 6; i ++) {console.log("el numero es: "+ i)}
+
+
+
+
+saludar("juan ");
+saludar("pedro ")
+console.log("sigma papiador " + nombre)
